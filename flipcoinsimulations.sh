f95e7ba419f8e521083f08d301b71e73ebc8d5fd
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-27 22:42:00 +0530

    Added code for looping multiple times of flipping a coin and showing no of times head and tail won

--- flipcoinsimulations.sh
+++ flipcoinsimulations.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-coin_flip_result=$(( RANDOM % 2 ))
-if(( coin_flip_result == 0 ))
+read -p "Enter no of times to flip a coin: " no_of_flips
+no_of_heads=0
+no_of_tails=0
+for(( i = 1; i <= no_of_flips; i++ ))
+do
+    coin_flip_result=$(( RANDOM % 2 ))
+    if(( coin_flip_result == 0 ))
+    then
+        echo "Flip-$i Heads"
+        (( no_of_heads++ ))
+    else
+        echo "Flip-$i Tails"
+        (( no_of_tails++ ))
+    fi 
+done
+echo "The no of Heads are is $no_of_heads and no of Tails are is $no_of_tails"
+if(( no_of_heads > no_of_tails ))
 then
-    echo "Heads"
-else
-    echo "Tails"
+echo "Heads Won"
+elif(( no_of_tails > no_of_heads ))
+then
+    echo "Tails Won"
+else    
+    echo "game is a tie"
 fi
